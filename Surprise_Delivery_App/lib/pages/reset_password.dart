5a93b2a79d997b4c8dc47db0f2e08c85636767ff
xjessxx <jessica.mcilree@mymail.champlain.edu>
2025-02-08 14:29:12 -0500

    brand colors

--- Surprise_Delivery_App/lib/pages/reset_password.dart
+++ Surprise_Delivery_App/lib/pages/reset_password.dart
@@ -30,9 +30,9 @@ class _ResetPasswordState extends State<ResetPassword> {
           height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
               gradient: LinearGradient(colors: [
-            hexStringToColor("CB2B93"),
-            hexStringToColor("9546C4"),
-            hexStringToColor("5E61F4")
+            hexStringToColor("faf31f"),
+            hexStringToColor("faae1f"),
+            hexStringToColor("fa7f1f")
           ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
           child: SingleChildScrollView(
               child: Padding(
--- Surprise_Delivery_App/lib/pages/signup_page.dart
+++ Surprise_Delivery_App/lib/pages/signup_page.dart
@@ -34,9 +34,9 @@ class _SignUpPageState extends State<SignUpPage> {
           height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
               gradient: LinearGradient(colors: [
-            hexStringToColor("CB2B93"),
-            hexStringToColor("9546C4"),
-            hexStringToColor("5E61F4")
+            hexStringToColor("faf31f"),
+            hexStringToColor("faae1f"),
+            hexStringToColor("faf31f")
           ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
           child: SingleChildScrollView(
               child: Padding(
