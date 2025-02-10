301a7a4dd27f57111f25dcf474ead0ffd40479c4
LGugs3 <liam.gugliotta@mymail.champlain.edu>
2025-02-10 13:35:42 -0500

    Fixed firebase not initializing problem. Don't know any creds

--- Surprise_Delivery_App/lib/main.dart
+++ Surprise_Delivery_App/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:surpirse_delivery_app/pages/signin_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: "assets/.firebaseAuth.env");
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
