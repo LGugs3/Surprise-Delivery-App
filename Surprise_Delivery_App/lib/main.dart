37657e2b262773fe7ca5c2fdb3fb60991ce593ae
Matthew <matthew.kanejimenez@mymail.champlain.edu>
2025-02-17 18:06:27 -0500

    Some changes I worked on

--- Surprise_Delivery_App/lib/main.dart
+++ Surprise_Delivery_App/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:surpirse_delivery_app/pages/signin_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+// testuser@gmail.com
+// testPass
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
