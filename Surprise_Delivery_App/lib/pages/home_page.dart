84a15de321a3a53d8730aff96937d07fa92472f0
xjessxx <jessica.mcilree@mymail.champlain.edu>
2025-02-19 18:24:09 -0500

    created start of order from

--- Surprise_Delivery_App/lib/pages/home_page.dart
+++ Surprise_Delivery_App/lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:surpirse_delivery_app/pages/signin_page.dart';
 import 'package:flutter/material.dart';
 import 'package:surpirse_delivery_app/pages/settings_page.dart';
+import 'package:surpirse_delivery_app/pages/order_form.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,18 +13,19 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-Row buttonRow(BuildContext context)
-{
+Row buttonRow(BuildContext context) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: <Widget>[
       Column(
         children: <Widget>[
           ElevatedButton(
-            child:
-            const Text("Place Order"),
+            child: const Text("Place Order"),
             onPressed: () {
-              print("Place Order button pressed.");
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => OrderForm()),
+              );
             },
           )
         ],
@@ -31,8 +33,7 @@ Row buttonRow(BuildContext context)
       Column(
         children: <Widget>[
           ElevatedButton(
-            child:
-            const Text("View Map"),
+            child: const Text("View Map"),
             onPressed: () {
               print("View Map button pressed.");
             },
@@ -68,22 +69,21 @@ class _HomePageState extends State<HomePage> {
           IconButton(
             icon: const Icon(Icons.logout),
             tooltip: 'Logout',
-            onPressed: (){
+            onPressed: () {
               FirebaseAuth.instance.signOut().then((value) {
                 print("Signed out");
                 Navigator.push(context,
-                MaterialPageRoute(builder: (context) => SignInPage()));
+                    MaterialPageRoute(builder: (context) => SignInPage()));
               });
             },
           )
         ],
       ),
       body: Center(
-        child:
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 50, 0, 50),
-            child: buttonRow(context),
-          ),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 50, 0, 50),
+          child: buttonRow(context),
+        ),
       ),
     );
   }
---
+++ Surprise_Delivery_App/lib/pages/order_form.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:surpirse_delivery_app/utils/color_utils.dart';
+import 'package:surpirse_delivery_app/pages/home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:multi_select_flutter/multi_select_flutter.dart';
+
+class OrderForm extends StatefulWidget {
+  const OrderForm({super.key});
+
+  @override
+  State<OrderForm> createState() => _OrderFormState();
+}
+
+class _OrderFormState extends State<OrderForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Help Us Pick!",
+            style: TextStyle(color: const Color.fromARGB(255, 155, 111, 8)),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.orangeAccent.withValues(alpha: 0.5),
+        ),
+        body: Container(
+            padding: EdgeInsets.all(8.0),
+            color: Colors.orange.shade200,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Expanded(child: Text("main")),
+                Expanded(child: Text("side")),
+                Expanded(child: Text("drink")),
+                Expanded(child: Text("dessert"))
+              ],
+            )));
+  }
+}
