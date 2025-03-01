7a01d7fed9199f0b0e3bf0274aa980df674a1518
xjessxx <jessica.mcilree@mymail.champlain.edu>
2025-03-01 16:48:09 -0500

    fonts for gen ref

--- Surprise_Delivery_App/lib/pages/order_form.dart
+++ Surprise_Delivery_App/lib/pages/order_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:multi_select_flutter/multi_select_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 class OrderForm extends StatefulWidget {
   const OrderForm({super.key});
@@ -9,28 +10,196 @@ class OrderForm extends StatefulWidget {
 }
 
 class _OrderFormState extends State<OrderForm> {
+  //allergies list
+  final List<String> allergiesOptions = [
+    'Peanuts',
+    'Tree Nuts',
+    'Dairy',
+    'Sesame',
+    'Soy',
+    'Shellfish',
+    'Fish',
+    'Eggs'
+  ];
+
+  //dietary restrictions list
+  final List<String> dietaryRestrictionsOptions = [
+    'Vegetarian',
+    'Vegan',
+    'Halal',
+    'Kosher',
+    'Pescatarian',
+    'Gluten Free'
+  ];
+
+  // Selected items for allergies and dietary restrictions
+  List<String> _selectedAllergies = [];
+  List<String> _selectedDietaryRestrictions = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(
-            "Help Us Pick!",
-            style: TextStyle(color: const Color.fromARGB(255, 155, 111, 8)),
+          title: Stack(
+            children: [
+              // Black border (outline effect)
+              Text(
+                "Help Us Pick!",
+                style: GoogleFonts.lilitaOne(
+                  fontSize: 30,
+                  foreground: Paint()
+                    ..style = PaintingStyle.stroke
+                    ..strokeWidth = 4
+                    ..color = Colors.black, // Border color
+                ),
+              ),
+              // Main text with color
+              Text(
+                "Help Us Pick!",
+                style: GoogleFonts.lilitaOne(
+                  fontSize: 30,
+                  color: Colors.orange.shade400,
+                ),
+              ),
+            ],
           ),
           centerTitle: true,
-          backgroundColor: Colors.orangeAccent.withValues(alpha: 0.5),
+          backgroundColor:
+              const Color.fromARGB(255, 239, 214, 29).withValues(alpha: 0.5),
         ),
         body: Container(
-            padding: EdgeInsets.all(8.0),
-            color: Colors.orange.shade200,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          padding: EdgeInsets.all(8.0),
+          color: Colors.orange.shade200,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Aligns text inside to the left
               children: <Widget>[
-                Expanded(child: Text("main")),
-                Expanded(child: Text("side")),
-                Expanded(child: Text("drink")),
-                Expanded(child: Text("dessert"))
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    padding: EdgeInsets.all(0.8),
+                    color: Colors.pink.shade50,
+                    child: Text(
+                      "Anything we should know for this meal?",
+                      style: GoogleFonts.lilitaOne(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                // Dropdown for Allergies
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    // Dropdown for Allergies
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 4.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width *
+                            0.4, // Set width to 40% of the screen
+                        child: MultiSelectDialogField(
+                          items: allergiesOptions
+                              .map((allergy) =>
+                                  MultiSelectItem<String>(allergy, allergy))
+                              .toList(),
+                          title: Text("Select Allergies"),
+                          selectedColor: Colors.orange.shade400,
+                          buttonText: Text(
+                            "Allergies",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          onConfirm: (selectedValues) {
+                            setState(() {
+                              _selectedAllergies =
+                                  selectedValues.cast<String>();
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    // Dropdown for Dietary Restrictions
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 4.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width *
+                            0.4, // Set width to 40% of the screen
+                        child: MultiSelectDialogField(
+                          items: dietaryRestrictionsOptions
+                              .map((restriction) => MultiSelectItem<String>(
+                                  restriction, restriction))
+                              .toList(),
+                          title: Text("Select Dietary Restrictions"),
+                          selectedColor: Colors.orange.shade400,
+                          buttonText: Text(
+                            "Dietary Restrictions",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          onConfirm: (selectedValues) {
+                            setState(() {
+                              _selectedDietaryRestrictions =
+                                  selectedValues.cast<String>();
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 12),
+                SizedBox(height: 12),
+                Container(
+                  width: 100,
+                  height: 80,
+                  alignment: Alignment.center,
+                  color: Colors.orange.shade400,
+                  child: Text(
+                    "main:",
+                    style: GoogleFonts.lilitaOne(fontSize: 24),
+                  ),
+                ),
+                SizedBox(height: 12),
+                Container(
+                  width: 100,
+                  height: 80,
+                  alignment: Alignment.center,
+                  color: Colors.orange.shade400,
+                  child: Text(
+                    "side:",
+                    style: GoogleFonts.lilitaOne(fontSize: 24),
+                  ),
+                ),
+                SizedBox(height: 12),
+                Container(
+                  width: 100,
+                  height: 80,
+                  alignment: Alignment.center,
+                  color: Colors.orange.shade400,
+                  child: Text(
+                    "drink:",
+                    style: GoogleFonts.lilitaOne(fontSize: 24),
+                  ),
+                ),
+                SizedBox(height: 12),
+                Container(
+                  width: 100,
+                  height: 80,
+                  alignment: Alignment.center,
+                  color: Colors.orange.shade400,
+                  child: Text(
+                    "dessert:",
+                    style: GoogleFonts.lilitaOne(fontSize: 24),
+                  ),
+                ),
               ],
-            )));
+            ),
+          ),
+        ));
   }
 }
