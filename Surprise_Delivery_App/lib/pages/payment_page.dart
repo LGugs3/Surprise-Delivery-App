ab48fd4f449bfdf2bb14318f1a0474ce1253e29d
xjessxx <jessica.mcilree@mymail.champlain.edu>
2025-04-16 19:18:44 -0400

    added receipt page and moved order generation logic so it is displayed

--- Surprise_Delivery_App/lib/pages/payment_page.dart
+++ Surprise_Delivery_App/lib/pages/payment_page.dart
@@ -1,13 +1,8 @@
-import 'dart:convert';
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:surpirse_delivery_app/utils/color_utils.dart';
-import 'package:surpirse_delivery_app/pages/home_page.dart';
-import 'package:surpirse_delivery_app/reusable_widgets/meal_class.dart';
+import 'package:surpirse_delivery_app/pages/reciept_page.dart';
 import 'package:surpirse_delivery_app/reusable_widgets/order_data_class.dart';
-import 'package:surpirse_delivery_app/reusable_widgets/resolved_order_class.dart';
-import 'package:http/http.dart' as http;
 
 class Payment extends StatefulWidget {
   const Payment({super.key, required this.orderData});
@@ -252,8 +247,8 @@ class _PaymentState extends State<Payment> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) =>
-                                HomePage()), // will be changed to some sort of order processing/delivery page
+                          builder: (context) => Reciept(orderData: orderData),
+                        ),
                       );
                     }
                   },
@@ -272,131 +267,5 @@ class _PaymentState extends State<Payment> {
         ),
       ),
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      //this needs to be moved to the page the user is directed to
-      //after this one(including the functions)
-      fetchPotentialMeals();
-    });
-  }
-
-  Future<Map<String, dynamic>?> getMealByID(String id) async
-  {
-    final http.Response response = await http.get(Uri.parse("https://www.themealdb.com/api/json/v1/1/lookup.php?i=$id"));
-
-    if (response.statusCode != 200){
-      print("Bad http request; error ${response.statusCode}");
-      return null;
-    }
-    else{
-      Map<String, dynamic> decoded = jsonDecode(response.body) as Map<String, dynamic>;
-      return decoded["meals"][0];
-    }
-  }
-
-  void fetchPotentialMeals() async
-  {
-    Set<String> warningsList = {};
-
-    if (orderData.cuisineSelection == "Random"){
-      Random rng = Random();
-      //-1 to exclude "random" option
-      orderData.cuisineSelection = cuisineOptions[rng.nextInt(cuisineOptions.length - 1)];
-    }
-    final Uri apiUri = Uri.parse("https://www.themealdb.com/api/json/v1/1/filter.php?a=${orderData.cuisineSelection}");
-    final http.Response response = await http.get(apiUri);
-
-    if (response.statusCode != 200) {
-      //replace with ui element that says "we couldnt find your order"
-      print("Bad http request; error ${response.statusCode}");
-      warningsList.add("Failed to fetch meal database");
-    }
-    else
-    {
-      Map<String, dynamic> cuisineCategory = jsonDecode(response.body) as Map<String, dynamic>;
-
-      //print((await getMealByID(cuisineCategory["meals"][0]["idMeal"]))?.keys);
-      List<Map<String, dynamic>> mainMeals = List<Map<String, dynamic>>.empty(growable: true);
-      List<Map<String, dynamic>> sideMeals = List<Map<String, dynamic>>.empty(growable: true);
-      List<Map<String, dynamic>> dessertMeals = List<Map<String, dynamic>>.empty(growable: true);
-
-      if (cuisineCategory.isEmpty || cuisineCategory["meals"] == null){
-        print("fetched db is empty");
-        warningsList.add("Fetched database is empty");
-        return;
-      }
-      for(Map<String, dynamic> meal in cuisineCategory["meals"]){
-        Map<String, dynamic>? nextMeal = await getMealByID(meal["idMeal"]);
-        if (nextMeal == null){
-          warningsList.add("some meals were not able to be fetched");
-          continue;
-        }
-        else{
-          if (nextMeal.isEmpty){
-            warningsList.add("some meals were not able to be fetched");
-            continue;
-          }
-          //add meal to one of the three lists
-          String nextCat = nextMeal["strCategory"];
-          if(nextCat == "Dessert"){
-            dessertMeals.add(nextMeal);
-          }
-          else if (nextCat == "Side"){
-            sideMeals.add(nextMeal);
-          }
-          else{
-            mainMeals.add(nextMeal);
-          }
-        }
-      }
-      buildOrder(dessertMeals, sideMeals, mainMeals, warningsList);
-    }
-  }
-
-  List<String> addMealOfType(List<Map<String, dynamic>> mealList, int mealCount, Set<String> warningsList)
-  {
-    List<String> returnedMeals = List<String>.empty(growable: true);
-    Random rng = Random();
-    for(int i = 0; i < mealCount; i++){
-      if(mealList.isEmpty){
-        warningsList.add("Not able to fetch proper number of meals due to request number was too high.");
-        break;
-      }
-      int nextRng = rng.nextInt(mealList.length);
-      returnedMeals.add(mealList[nextRng]["strMeal"]);
-      mealList.removeAt(nextRng);
-    }
-    return returnedMeals;
-  }
-
-  void buildOrder(List<Map<String, dynamic>> dessertMeals,
-                  List<Map<String, dynamic>> sideMeals,
-                  List<Map<String, dynamic>> mainMeals,
-                  Set<String> warningsList)
-  {
-    //check for dietary restrictions in seperate func here
-    //remove meals with conflicting dietary restrictions/allergies in that func
-    //add one more parameter for drinks
-    ResolvedOrder finalOrder = ResolvedOrder();
-    for(Meal meal in orderData.orderedMeals){
-      ResolvedMeal finalMeal = finalOrder.add();
-
-      finalMeal.mainMeals.addAll(addMealOfType(mainMeals, meal.mainCount, warningsList));
-      finalMeal.sideMeals.addAll(addMealOfType(sideMeals, meal.sideCount, warningsList));
-      finalMeal.dessertMeals.addAll(addMealOfType(dessertMeals, meal.dessertCount, warningsList));
-
-    }
-
-    //desired output for user
-    for(ResolvedMeal finalMeal in finalOrder.meals){
-      print(finalMeal.mainMeals);
-      print(finalMeal.sideMeals);
-      print(finalMeal.dessertMeals);
-    }
-    print(warningsList);
   }
 }
